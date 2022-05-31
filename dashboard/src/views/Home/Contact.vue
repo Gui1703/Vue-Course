d13bceb243c4db4ae6c97e3d49bdd6de458ed439
Guilherme Raposo <gui.raposo36@gmail.com>
2022-05-30 21:03:22 -0300

    feat: finished home screen

---
+++ dashboard/src/views/Home/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="flex justify-center w-full">
+    <main class="flex flex-col items-center w-4/5 max-w-6xl my-16">
+      <h1 class="text-4xl font-black text-center text-gray-800">
+        Alguma dúvida ?
+      </h1>
+      <p class="text-lg text-center text-gray-800 font-regular">
+        Quer saber melhor como funciona e quais são os preços ?
+      </p>
+      <button
+        class="mt-10 px-6 py-2 font-bold text-white rounded-full border-none bg-brand-main focus:outline-none cursor-pointer hover:opacity-80"
+      >
+        Nos mande um e-mail
+      </button>
+    </main>
+  </section>
+</template>
+<script>
+export default {}
+</script>
--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>
-    <h1 class="text-brand-main">Home, aqui é a home</h1>
-  </div>
+  <CustomHeader @create-account="handleAccountCreate" @login="handleLogin" />
+  <Contact />
+
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">Feedbacker &copy; 2022</p>
+  </footer>
 </template>
+
+<script>
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+import { onMounted } from '@vue/runtime-core'
+import { useRouter } from 'vue-router'
+export default {
+  components: {
+    CustomHeader,
+    Contact
+  },
+
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = localStorage.getItem('token')
+
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {}
+
+    function handleAccountCreate () {}
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
+</script>

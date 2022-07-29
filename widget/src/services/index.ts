441cfe8ef29a3f90c6b3af02d2413762635d30f5
Guilherme Raposo <gui.raposo36@gmail.com>
2022-07-29 09:34:21 -0300

    feat: created first commit widget

---
+++ widget/src/services/index.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const API_ENVS = {
+  production: 'https://backend-vue-course-dgkzh2yok-gui1703.vercel.app/',
+  development: '',
+  local: 'http://localhost:3000'
+}
+
+const httpClient = axios.create({
+  baseURL: API_ENVS[process.env.NODE_ENV] || API_ENVS.local
+})
+
+httpClient.interceptors.response.use(
+  (response) => {
+    return response
+  },
+
+  (error) => {
+    const canThrowAnError =
+      error.request.error === 0 || error.request.status === 500
+
+    if (canThrowAnError) {
+      throw new Error(error.message)
+    }
+
+    return error
+  }
+)
+
+export default {
+
+}
